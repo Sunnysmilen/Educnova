fddc2cd5bd69adab7d8ec9d0926f528ba99259de
sunnysmilen <davidsalnicole@gmail.com>
2025-05-01 14:15:01 +0200

    feat: creation of footer

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import { NavLink } from "react-router";
 import educnova from "../assets/images/EducNova.png";
 import "../assets/styles/navbar.css";
 
 function navbar() {
   return (
     <nav>
+      <NavLink to="/Accueil">Accueil</NavLink>
+      <NavLink to="/Etablissement">Etablissement</NavLink>
+      <NavLink to="/Equipe scolaire">Equipe scolaire</NavLink>
+      <NavLink to="/Calendrier scolaire">Calendrier scolaire</NavLink>
+      <NavLink to="/Programmes">Programmes</NavLink>
+
       <img src={educnova} alt="Logo Educnova" className="educnova" />
     </nav>
   );
